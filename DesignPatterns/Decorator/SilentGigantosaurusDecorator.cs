847c04fbf3e7c607a6236b7aec85d50f18baef4c
Carmel Eve <Carmel.Eve@endjin.com>
2020-10-12 10:01:33 +0100

    Added SilentGigantosaurusDecorator

---
+++ DesignPatterns/Decorator/SilentGigantosaurusDecorator.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DesignPatterns.Decorator
+{
+    public class SilentGigantosaurusDecorator : IGigantosaurus
+    {
+        protected readonly IGigantosaurus gigantosaurus;
+
+        public SilentGigantosaurusDecorator(IGigantosaurus gigantosaurus)
+        {
+            this.gigantosaurus = gigantosaurus;
+        }
+
+        public string Roar()
+        {
+            return "";
+        }
+    }
+}
